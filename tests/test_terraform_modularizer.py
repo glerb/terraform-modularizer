1c991fa0f7a4249a285ff2c6c6065453d6989e43
Bart de Goede <bart@scribd.com>
2020-06-09 15:15:14 -0700

    Compare to expected result

--- tests/test_terraform_modularizer.py
+++ tests/test_terraform_modularizer.py
@@ -1,14 +1,33 @@
 import unittest
 import sys
 sys.path.append('./src')
-from terraform_modularizer.modularize import *
+from terraform_modularizer.modularize import parsehcl
 from terraform_modularizer.help_messages import help_messages
 
 class TestTerraformModularizerMethods(unittest.TestCase):
 
     def test_parsehcl_success(self):
         """parsehcl succeeds with a valid terraform file"""
-        self.assertIsInstance(parsehcl('tests/test_good.tf'), dict)
+        expected_data = {
+            'resource': [{
+                'aws_instance': {
+                    'the_machine': {
+                        'ami': ['ami-12345'],
+                        'instance_type': ['t2.micro'],
+                        'availability_zone': ['us-east-1a'],
+                        'subnet_id': ['subnet-11zxcvxcv'],
+                        'vpc_security_group_ids': [[
+                            '${aws_security_group.the_group.id}',
+                            '${aws_security_group.the_other_group.id}'
+                        ]],
+                        'tags': [{'Terraform': True}]
+                    }
+                }
+            }]
+        }
+        parsed_data = parsehcl('tests/test_good.tf')
+        self.assertIsInstance(parsed_data, dict)
+        self.assertEqual(expected_data, parsed_data)
 
     def test_parsehcl_filenotfound(self):
         """parsehcl fails when the file does not exist. The following text is the error message a user sees:"""
